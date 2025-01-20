5cc5367701bb8bde398987d8cb61160dfbaf36b5
Bart v. Eijkelenburg <bart.vaneijkelenburg@hu.nl>
2025-01-20 20:57:18 +0100

    Initial commit

---
+++ hu_dashboard/engine.py
@@ -0,0 +1,6 @@
+def my_print():
+    print('Hi, hello world!')
+
+
+def my_print_param(param):
+    print(param)
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hu-dashboard',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # Voeg hier afhankelijkheden toe
+    ],
+    author='Bart van Eijkelenburg',
+    author_email='bart.vaneijkelenburg@hu.nl',
+    description='Dashboard Engine HBO-ICT first semester',
+    url='https://github.com/bveijkelenburg/hu-dashboard',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
