4ffb57c76bfc430c4d1ba6f7a377d8c5ef776e63
Bart v. Eijkelenburg <bart.vaneijkelenburg@hu.nl>
2025-01-20 21:02:02 +0100

    Renamed hu-dashboard to hu_dashboard

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='hu-dashboard',
+    name='hu_dashboard',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
